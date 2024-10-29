b658075bba45d5a82385eb73db1b108fcceb3d4d
aadiityaak <aadiityaak@gmail.com>
2024-10-29 08:12:02 +0700

    tambah table customers

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Container\Attributes\Auth;
 
@@ -10,7 +10,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('posts', PostController::class);
+Route::apiResource('customers', CustomerController::class)->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
