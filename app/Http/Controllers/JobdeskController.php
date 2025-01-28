4921ae4cc9f77491c7ecd65bffdfbdd2ef823fb8
aadiityaak <aadiityaak@gmail.com>
2025-01-28 22:05:14 +0700

    fix jobdesk controller

--- app/Http/Controllers/JobdeskController.php
+++ app/Http/Controllers/JobdeskController.php
@@ -39,7 +39,7 @@ class JobdeskController extends Controller
         }
 
         // Paginate the results
-        $jobdesk = $query->paginate(25);
+        $jobdesk = $query->orderBy('id', 'desc')->paginate(25);
 
         return response()->json($jobdesk);
     }
