3be89da55555f94721b0f5500bd51a6444867574
aadiityaak <aadiityaak@gmail.com>
2025-02-25 14:35:58 +0700

    perapian route api

--- routes/api.php
+++ routes/api.php
@@ -2,58 +2,72 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\KaryawanController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\JobdeskController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\SettingController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\MetaController;
-use App\Http\Controllers\SendNotificationController;
-use App\Http\Controllers\Auth\ProfileController;
-use App\Http\Controllers\NotificationController;
-use App\Http\Controllers\PermissionController;
-use App\Http\Controllers\SettingBankController;
-use App\Http\Controllers\SettingBackgroundController;
-use App\Http\Controllers\SettingFaviconController;
+use App\Http\Controllers\{
+    KaryawanController,
+    OrderController,
+    JobdeskController,
+    CustomerController,
+    DashboardController,
+    SettingController,
+    UserController,
+    ProductController,
+    MetaController,
+    SendNotificationController,
+    NotificationController,
+    PermissionController,
+    SettingBankController,
+    SettingBackgroundController,
+    SettingFaviconController,
+    Auth\ProfileController
+};
 
+// Routes tanpa middleware
 Route::get('/settings/background', [SettingBackgroundController::class, 'index']);
 Route::get('/settings/favicon', [SettingFaviconController::class, 'index']);
 
+// Routes dengan middleware 'auth:sanctum'
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('profile', function (Request $request) {
-        return $request->user();
-    });
+    Route::get('profile', fn(Request $request) => $request->user());
     Route::get('user', [UserController::class, 'index']);
-    Route::post('jobdesk-reminder', [SendNotificationController::class, 'sendJobdeskReminder']);
     Route::put('profile', [ProfileController::class, 'update']);
     Route::get('home', [DashboardController::class, 'index']);
+    Route::post('jobdesk-reminder', [SendNotificationController::class, 'sendJobdeskReminder']);
 
-    Route::get('notifications', [NotificationController::class, 'index']);
-    Route::put('notifications/{id}', [NotificationController::class, 'update']);
-    Route::put('notifications/read/{id}', [NotificationController::class, 'read']);
-    Route::put('notifications/read-all', [NotificationController::class, 'readAll']);
-    Route::get('notifications/unread', [NotificationController::class, 'unread']);
+    // Notifications
+    Route::controller(NotificationController::class)->group(function () {
+        Route::get('notifications', 'index');
+        Route::put('notifications/{id}', 'update');
+        Route::put('notifications/read/{id}', 'read');
+        Route::put('notifications/read-all', 'readAll');
+        Route::get('notifications/unread', 'unread');
+    });
 
+    // Permissions
     Route::get('/permissions', [PermissionController::class, 'index']);
     Route::post('/user/{user}/permissions', [PermissionController::class, 'update']);
 
+    // Settings
     Route::prefix('settings')->group(function () {
-        Route::post('/background', [SettingBackgroundController::class, 'store']);
-        Route::delete('/background', [SettingBackgroundController::class, 'destroy']);
+        Route::controller(SettingBackgroundController::class)->group(function () {
+            Route::post('/background', 'store');
+            Route::delete('/background', 'destroy');
+        });
 
-        Route::post('/favicon', [SettingFaviconController::class, 'store']);
-        Route::delete('/favicon', [SettingFaviconController::class, 'destroy']);
+        Route::controller(SettingFaviconController::class)->group(function () {
+            Route::post('/favicon', 'store');
+            Route::delete('/favicon', 'destroy');
+        });
 
-        Route::get('/banks', [SettingBankController::class, 'index']);
-        Route::post('/banks', [SettingBankController::class, 'store']);
-        Route::delete('/banks', [SettingBankController::class, 'destroy']);
+        Route::controller(SettingBankController::class)->group(function () {
+            Route::get('/banks', 'index');
+            Route::post('/banks', 'store');
+            Route::delete('/banks', 'destroy');
+        });
     });
 
     Route::post('customers/{customer}/meta', [CustomerController::class, 'storeMeta']);
 
+    // API Resources
     Route::apiResources([
         'karyawans' => KaryawanController::class,
         'orders' => OrderController::class,
