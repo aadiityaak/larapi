59f45a33df1c0b165cf693b21fdd603e6b36e1e4
aadiityaak <aadiityaak@gmail.com>
2025-05-23 19:42:29 +0700

    remove jobdesk factory

---
+++ app/Models/Jobdesk.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Jobdesk extends Model
+{
+  use HasFactory;
+
+  protected $fillable = [
+    'order_id',
+    'user_id',
+    'description',
+    'tanggal_pengerjaan',
+    'tanggal_selesai',
+    'status',
+  ];
+
+  protected $casts = [
+    'tanggal_pengerjaan' => 'date', // Automatic casting to date
+    'tanggal_selesai' => 'date', // Automatic casting to date
+  ];
+
+  public function order(): BelongsTo
+  {
+    return $this->belongsTo(Order::class);
+  }
+
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class);
+  }
+}
